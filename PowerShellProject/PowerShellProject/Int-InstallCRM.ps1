08319833efa389a4fbf023ee1a086a511004f3dd
arimaze <adrian.foata@gmail.com>
2015-09-03 10:40:30 +0200

    finalize install

--- PowerShellProject/PowerShellProject/Int-InstallCRM.ps1
+++ PowerShellProject/PowerShellProject/Int-InstallCRM.ps1
@@ -1,5 +1,6 @@
 #
 # InstallCRM.ps1
+# prerequisites : sandbox solution started on 2 applications server 03 & 04
 #
 New-SPContentDatabase "BB2013_Int_CRM_Content" -DatabaseServer "MRSSPI01\SPI" -WebApplication https://intranet-int.bourbon-online.com -MaxSiteCount 5000 -WarningSiteCount 2000
 New-SPManagedPath "crm" -WebApplication "https://intranet-int.bourbon-online.com" -Explicit
@@ -12,3 +13,12 @@ $Template = "BourbonSite#2"
 $PrimaryLogin = "bbint\svc.wssadmin-int"
 
 New-SPSite -Url $url –ContentDatabase $ContentDatabase -Name $WebsiteName –Description $WebsiteDesc  -Template $Template -OwnerAlias $PrimaryLogin
+
+#STEP 2 add the solution to the site collection via GUI
+
+#STEP 3 allow .htc files to the web app PORTAL-443 running this script
+C:\Distrib\crm\AllowHtcExtn.ps1 "https://intranet-int.bourbon-online.com"
+
+#STEP 4 IISreset all servers in the farm
+?C:\Users\svc.wssadmin-int\Desktop\PS_DoIISReset_AllServersInFarm.ps1
+
