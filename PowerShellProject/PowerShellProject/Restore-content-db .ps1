5d04a7018eab2236b837a12c6685cd376938aaae
arimaze <adrian.foata@gmail.com>
2015-09-10 13:21:36 +0200

    fix

--- PowerShellProject/PowerShellProject/Restore-content-db .ps1
+++ PowerShellProject/PowerShellProject/Restore-content-db .ps1
@@ -2,9 +2,15 @@ Dismount-SPContentDatabase -Identity BB2013_PreProd_Publishing_Content
 
 
 RESTORE DATABASE [BB2013_PreProd_Publishing_Content] 
-FROM DISK = 'C:\Users\svc.wssadmin-pre\Desktop\BB2013_Prod_Publishing_Content.bak' WITH  FILE = 1,  NOUNLOAD,  STATS = 10,
+FROm disk = 't:\BB2013_Prod_Publishing_Content.bak' WITH FILE = 1,  NOUNLOAD, STATS = 10,
 MOVE N'BB2013_Prod_Publishing_Content' To 'S:\data\MSSQL11.SPPP\MSSQL\DATA\BB2013_PreProd_Publishing_Content.mdf',
 MOVE N'BB2013_Prod_Publishing_Content_log' To 'S:\log\MSSQL11.SPPP\MSSQL\Data\BB2013_PreProd_Publishing_Content_log.LDF'
+GO
+
+RESTORE DATABASE [BB2013_PreProd_Portal_Content] 
+FROm disk = 't:\BB2013_Prod_Portal_Content.bak' WITH FILE = 1,  NOUNLOAD, STATS = 10,
+MOVE N'Portal_Content' To 'S:\data\MSSQL11.SPPP\MSSQL\DATA\BB2013_PreProd_Portal_Content.mdf',
+MOVE N'Portal_Content_log' To 'S:\log\MSSQL11.SPPP\MSSQL\Data\BB2013_PreProd_Portal_Content_log.LDF'
 GO
 
 CREATE USER [BOURBON\svc.wssapp-pre] FOR LOGIN [BOURBON\svc.wssapp-pre] WITH DEFAULT_SCHEMA=[BOURBON\svc.wssapp-pre]
