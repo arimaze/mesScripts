384410a1a1ef0ecc87a70c1cdb38a51be5f0cbe7
arimaze <adrian.foata@gmail.com>
2015-09-18 16:25:45 +0200

    final pré prod j'espere !

--- PowerShellProject/PowerShellProject/Refonte-PreProd.ps1
+++ PowerShellProject/PowerShellProject/Refonte-PreProd.ps1
@@ -23,33 +23,25 @@ ORDER BY
 
 #Move my dashboards
 New-SPContentDatabase "BB2013_PreProd_Dashboards_Content" -DatabaseServer "MRSSPSSPPP\SPPP" -WebApplication https://intranet-pre.bourbon-online.com -MaxSiteCount 5000 -WarningSiteCount 2000
+#Ad database to AG
 Move-SPSite https://intranet-pre.bourbon-online.com/mydashboards -DestinationDatabase BB2013_PreProd_Dashboards_Content
 Get-SPSite -ContentDatabase BB2013_PreProd_Portal_Content
 Get-SPSite -ContentDatabase BB2013_PreProd_Dashboards_Content
 get-spcontentdatabase | add-spshelladmin bourbon\svc.bippfront
-#Ad database to AG
+
 
 
 New-SPContentDatabase "BB2013_PreProd_Quality_Content" -DatabaseServer "MRSSPSSPPP\SPPP" -WebApplication https://intranet-pre.bourbon-online.com -MaxSiteCount 5000 -WarningSiteCount 2000
+#Add database to AG
 #mdf : 81920 mb and 10240 autogrow
 #ldf : 5120 mb and 10 percent
-#Add database to AG
-
-#Check autogrow for this new content database
 New-SPManagedPath "qa" -WebApplication "https://intranet-pre.bourbon-online.com" -Explicit
 get-spcontentdatabase | add-spshelladmin bourbon\svc.bippfront
-
 Backup-SPSite -Identity https://intranet-pre.bourbon-online.com -Path \\mrssps102\t$\racine.bak
-
-
-
-
-
 Restore-SPSite https://intranet-pre.bourbon-online.com/qa -Path \\mrssps102\t$\racine.bak -ContentDatabase BB2013_PreProd_Quality_Content -force
 
 
 #Check site and execute powershells script to correct
-
 #Change all /QA/QMS layouts via powershell script
 #CHange masterpage /qa to bourbon 2011 home
 #change masterpage /qa/qms Bourbon-2011 qms
@@ -60,44 +52,35 @@ Restore-SPSite https://intranet-pre.bourbon-online.com/qa -Path \\mrssps102\t$\r
 
 
 #On MRSSPS104
-
 #check orphans
 $db=get-spcontentdatabase bb2013_PreProd_publishing_content
 $db.repair($false)
 
-
-
 Backup-SPSite -Identity https://intranet-pre.bourbon-online.com/sites/refonteintranet -Path c:\refonte.bak
 
-# check autogrow publishing and portal
-# change log size publishing to 108 go (total size when removing site collection) . if no place ! hay hay hay
 # Disable FULL Backup all databases maintenance plan on nodes 101 & 102
+#copy .back of publishing and portal into the / of T disk to keep save to no be erased by tina
+#Shrink Publishing log file to save space
 
-#Add new log file to spp data because there are no space on sp log
-#publishing log file 100 go
-#Portal log file 180
-Remove-SPSite https://intranet-pre.bourbon-online.com/sites/refonteintranet
-Remove-SPSite https://intranet-pre.bourbon-online.com
+Dismount-SPContentDatabase BB2013_PreProd_Portal_Content
 
-#check SPDeletedSite
-#gradual delete
+New-SPContentDatabase "BB2013_PreProd_Publish_Content" -DatabaseServer "MRSSPSSPPP\SPPP" -WebApplication https://intranet-pre.bourbon-online.com -MaxSiteCount 5000 -WarningSiteCount 2000
+# Add database to AG
+# Change size and autogrow and add new log file to s:data because there are no space
 
 #IIS reset all servers via .PS1
 
 
-#check free SQL space Portal log file will increase. Publishing too even simple recovery mode
-#Resto
-Restore-SPSite https://intranet-pre.bourbon-online.com -Path c:\refonte.bak -ContentDatabase BB2013_PreProd_Publishing_Content -force
+Restore-SPSite https://intranet-pre.bourbon-online.com -Path c:\refonte.bak -ContentDatabase BB2013_PreProd_Publish_Content -force
  
 
 
 #Review crawl and delete "refonte intranet" or "publishing"
 #Full crawl "PORTAL" content source
-
 #change PORTAL to "continuous crawl"
-
-#Add DATABASES to AG
 #Enable FULL Backup all databases maintenance plan on nodes 101 & 102
+
+
 
 
 
